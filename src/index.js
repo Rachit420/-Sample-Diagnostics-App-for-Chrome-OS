9c9d765affb6934f0817b5b83bd0ce8849a5fca7
Rachit raj <65225697+Rachit420@users.noreply.github.com>
2021-03-23 17:26:38 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,15 @@
+if("serviceWorker" in navigator){
+    navigator.serviceWorker.register("sw.js").then(registration =>{
+        console.log("SW registerd");
+        console.log(registration);
+    }).catch(error =>{
+        console.log("sw registration failed");
+        console.log(error);
+    });
+}
+
+window.addEventListener("load", () => {
+  if ("serviceWorker" in navigator) {
+    navigator.serviceWorker.register("sw.js");
+  }
+});
